36b8d969594793b8ac1eccfba9f9fdc391578444
Xyron-22 <111947997+Xyron-22@users.noreply.github.com>
2023-04-26 16:36:43 +0800

    new update

--- controllers/user.js
+++ controllers/user.js
@@ -11,13 +11,13 @@ export const signin = async (req, res) => {
         const existingUser = await User.findOne({email});
 
         if(!existingUser) {
-            return res.status(404).json({ message: "User does not exist"});
+            return res.status(200).json({ message: "User does not exist"});//
         }
 
         const isPassCorrect = await bcrypt.compare(password, existingUser.password);
 
         if(!isPassCorrect) {
-            return res.status(400).json({message: "Invalid credentials"});
+            return res.status(200).json({message: "Invalid Credentials"});//
         }
 
         const token = jwt.sign({email: existingUser.email, id: existingUser._id}, process.env.SECRET_KEY, {expiresIn: "1h"});
@@ -36,10 +36,10 @@ export const signup = async (req, res) => {
         const exsistingUser = await User.findOne({email});
 
         if(exsistingUser) {
-            return res.status(400).json({message: "User already exist"});
+            return res.status(200).json({message: "User already exist"});
         }
         if(password !== confirmPassword) {
-            return res.status(400).json({message: "Password don't match"});
+            return res.status(200).json({message: "Password does not match"});
         }
         const hashedPassword = await bcrypt.hash(password, 12);
         const result = await User.create({
--- index.js
+++ index.js
@@ -9,7 +9,6 @@ dotenv.config();
 
 const app = express();
 
-
 app.use(express.urlencoded({extended: true, limit: "30mb"}));
 app.use(express.json({limit: "30mb"}));
 app.use(cors());
